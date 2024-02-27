f2b773ddaa2d9d58668804f21689597ca7b5c80e
stshryu <stshryu@gmail.com>
2024-02-26 19:39:20 -0500

    Starting fresh with docker images

--- generic_commands/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GenericCommandsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'generic_commands'
--- generic_commands/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from generic_commands import views
-
-urlpatterns = [
-    path('roll/', views.diceroll),
-    path('flip/', views.coinflip),
-]
