bcbc10c57720503db280cf80c6750b5938626208
stshryu <stshryu@gmail.com>
2024-03-09 00:28:33 -0500

    Middleware CORS wrap up

--- server/main_server/main.py
+++ server/main_server/main.py
@@ -52,6 +52,21 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
