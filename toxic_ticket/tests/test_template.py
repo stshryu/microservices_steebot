421a73d04f3fbcc897705a24f14fd819a665c5a2
stshryu <stshryu@gmail.com>
2024-03-27 00:26:20 -0400

    Adding test and boilerplate tests

---
+++ toxic_ticket/tests/test_template.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+import pytest
+from main import app, token_listener
+
+app.dependency_overries[token_listener] = lambda: {}
+client = TestClient(app)
+
+def test_api_base_api():
+    response = client.get("/")
+    assert response.status_code == 200
