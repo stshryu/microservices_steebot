d7c32d30e16514f7c8d2c0ca540de85dea76e822
stshryu <stshryu@gmail.com>
2024-04-11 01:47:07 -0400

    Adding success and error objects, creating toxic ticket validation

---
+++ discord_wrapper/errors.py
@@ -0,0 +1,20 @@
+import json
+
+class BaseError(Exception):
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+class UnexpectedError(BaseError):
+    """
+    Raise an unexpected error as our base error if something is uncaught
+    """
+    def __init__(self, message="UnexpectedError"):
+        self.message = message
+
+class InvalidDataInput(BaseError):
+    """
+    Raised when input data doesn't match the validation schema
+    """
+    def __init__(self, invalid_fields: dict):
+        formatted_errors = [f"{key}: {value}" for key, value in invalid_fields.items()]
+        self.message = '\n'.join(formatted_errors)
--- discord_wrapper/tasks/toxic_ticket_queue.py
+++ discord_wrapper/tasks/toxic_ticket_queue.py
@@ -1,11 +1,14 @@
 from fastapi import Depends
-from typing import Annotated, Dict 
+from typing import Annotated, Dict, get_type_hints
 from config.config import get_redis_connection
 import json
+import errors
+import success
 
 # Consider switching Any to an actually defined data type (for sending toxic ticket data)
 async def add_ticket_to_user(username: str, ticket_data: Dict[str, str]): 
-    await validate_ticket_data({"hi", "hello"})
+    test = {"username": "gavin", "ticket_type": "test", "amount": "test amount", "issuer": "test"}
+    res = await validate_ticket_data(test)
     return None
 
 async def validate_ticket_data(ticket_data: Dict[str, str]):
@@ -18,9 +21,10 @@ async def validate_ticket_data(ticket_data: Dict[str, str]):
     Returns:
         Object<ToxicTicket>
     """
-    valid_attributes = ToxicTicket.__fieldvalidation__
-    valid_object = { key: value for key, value in ticket_data if key in valid_attributes }
-    return ToxicTicket(**valid_object)
+    hints = get_type_hints(ToxicTicket.__init__)
+    ## TODO Finish method
+    valid_object = { key: value for key, value in ticket_data.items() if key in valid_attributes }
+    return None
 
 class ToxicTicket:
     __fieldvalidation__ = [
@@ -32,7 +36,7 @@ class ToxicTicket:
 
     def __init__(self, username: str, ticket_type: str, amount: int, issuer: str):
         self.username = username
-        selt.ticket_type = ticket_type
+        self.ticket_type = ticket_type
         self.amount = amount
         self.issuer = issuer
 
