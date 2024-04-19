5192756d7b8282d167c0dab8a47445ac30db359b
stshryu <stshryu@gmail.com>
2024-04-18 23:58:43 -0400

    Adding initial bot main logic

---
+++ discord_wrapper/services/discordMain.py
@@ -0,0 +1,12 @@
+import time
+import os
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
