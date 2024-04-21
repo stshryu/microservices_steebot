66fd3b216284782dea2b6162b682967daee2b056
stshryu <stshryu@gmail.com>
2024-04-20 21:58:06 -0400

    Adding skeleton discord wrapper

--- discord_wrapper/main.py
+++ discord_wrapper/main.py
@@ -4,11 +4,13 @@ import redis
 import asyncio
 from tasks.toxic_ticket_queue import *
 from services.subscriber import *
+from services.discordMain import *
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Ensure listener doesn't run on the same loop as the main thread
     asyncio.create_task(process_channel())
+    asyncio.create_task(bot_run())
     yield
 
 app = FastAPI(lifespan=lifespan)
--- discord_wrapper/services/discordMain.py
+++ discord_wrapper/services/discordMain.py
@@ -2,7 +2,8 @@ import time
 import os
 import discord
 from discord.ext import commands
-import random
+
+description = "Discord Server Wrapper"
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,3 +11,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 
+async def bot_run():
+    print(f"Starting bot")
+    try:
+        await bot.start("token")
+    except KeyboardInterrupt:
+        await bot.logout()
