c8981fde745a0ffe97bcea161ea7a1b7f65b95b6
stshryu <stshryu@gmail.com>
2024-05-12 00:12:56 -0400

    Fixing bug on asyncio loop

--- discord_wrapper/main.py
+++ discord_wrapper/main.py
@@ -4,17 +4,16 @@ import redis
 import asyncio
 from tasks.toxic_ticket_queue import *
 from services.subscriber import *
+from config.config import Settings
 from services.discordMain import *
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Ensure listener doesn't run on the same loop as the main thread
-    redis_queue = asyncio.create_task(process_channel())
-    discord_bot = asyncio.create_task(bot_run())
-    await redis_queue
-    await discord_bot
+    # Add to loop
+    asyncio.create_task(process_channel())
+    asyncio.create_task(main_loop())
     yield
-
+    
 app = FastAPI(lifespan=lifespan)
 
 @app.get("/", tags=["Root"])
--- discord_wrapper/services/discordMain.py
+++ discord_wrapper/services/discordMain.py
@@ -3,6 +3,7 @@ import os
 import discord
 from discord.ext import commands
 from config.config import Settings
+import asyncio
 
 description = "Discord Server Wrapper"
 
@@ -12,9 +13,22 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 
-async def bot_run():
-    print(f"Starting bot")
-    try:
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("-------")
+
+@bot.command()
+async def foo(ctx, arg):
+    print(f"{arg=}")
+    await ctx.send(arg)
+
+async def start_bot():
+    async with bot:
         await bot.start(Settings().DISCORD_TOKEN)
-    except KeyboardInterrupt:
-        await bot.logout()
+
+async def main_loop():
+    tasks = [
+        asyncio.create_task(start_bot())
+    ]
+    await asyncio.wait(tasks)
